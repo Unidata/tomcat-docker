90ca1412754d54aa3505128eaef5d0841023e5e4
Julien Chastang <julien.c.chastang@gmail.com>
2016-10-26 16:03:25 -0600

    simplifying permissions and nixing duplication

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,13 @@
 set -e
 
 if [ "$1" = 'catalina.sh' ]; then
-    chown -R tomcat:tomcat ${CATALINA_HOME} && \
-        chmod 400 ${CATALINA_HOME}/conf/* && \
-        chmod 300 ${CATALINA_HOME}/logs/.
+
+    ###
+    # Change CATALINA_HOME ownership to tomcat user and tomcat group
+    # Restrict permissions on conf
+    ###
+
+    chown -R tomcat:tomcat ${CATALINA_HOME} && chmod 400 ${CATALINA_HOME}/conf/*
     sync
     exec gosu tomcat "$@"
 fi
