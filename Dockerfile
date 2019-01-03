Unidata/tomcat-docker

commit f78110ee1e2cfc52299d1915249d5b07e5f8546b

obscuring server info

Dockerfile
@@ -23,15 +23,15 @@ RUN apt-get update && \
     ###
     # Obscuring server info
     ###
-    mkdir -p ${CATALINA_HOME}/lib/org/apache/catalina/util/ && \
-    unzip -j ${CATALINA_HOME}/lib/catalina.jar \
-        org/apache/catalina/util/ServerInfo.properties \
-        -d ${CATALINA_HOME}/lib/org/apache/catalina/util/ && \
+    pushd ${PWD} && cd ${CATALINA_HOME}/lib && \
+    mkdir -p org/apache/catalina/util/ && \
+    unzip -j catalina.jar org/apache/catalina/util/ServerInfo.properties \
+        -d org/apache/catalina/util/ && \
     sed -i 's/server.info=.*/server.info=Apache Tomcat/g' \
-        ${CATALINA_HOME}/lib/org/apache/catalina/util/ServerInfo.properties && \
-    zip -ur ${CATALINA_HOME}/lib/catalina.jar \
-        ${CATALINA_HOME}/lib/org/apache/catalina/util/ServerInfo.properties && \
-    rm -rf ${CATALINA_HOME}/lib/org && \
+        org/apache/catalina/util/ServerInfo.properties && \
+    zip -ur catalina.jar \
+        org/apache/catalina/util/ServerInfo.properties && \
+    rm -rf org && popd \
     sed -i 's/<Connector/<Connector server="Apache" secure="true"/g' \
         ${CATALINA_HOME}/conf/server.xml && \
     ###

commit f69bf5cae472360aa1193e80138433df728da17c

typo introduced in f78110e by moi. Thxs @mnoky.

Dockerfile
@@ -31,7 +31,7 @@ RUN apt-get update && \
         org/apache/catalina/util/ServerInfo.properties && \
     zip -ur catalina.jar \
         org/apache/catalina/util/ServerInfo.properties && \
-    rm -rf org && cd ${CATALINA_HOME} \
+    rm -rf org && cd ${CATALINA_HOME} && \
     sed -i 's/<Connector/<Connector server="Apache" secure="true"/g' \
         ${CATALINA_HOME}/conf/server.xml && \
     ###