dcdc712ae265d6746b6b4fc7b2406b1d3ff5d7c7
Julien Chastang <julien.c.chastang@gmail.com>
2021-09-15 18:17:21 -0600

    expunge travis

--- .travis/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -o ./.travis/actual.html http://127.0.0.1:8080/sample/index.html
-
-diff ./.travis/expected.html ./.travis/actual.html
-
-cmp ./.travis/expected.html ./.travis/actual.html
