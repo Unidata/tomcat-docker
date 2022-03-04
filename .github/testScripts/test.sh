69c1ba8531e4a76b36f0498d5f5edc1491b61359
Bobby Espinoza <respinoza@ucar.edu>
2022-03-04 10:12:27 -0700

    Implement Github Actions

--- .github/testScripts/test.sh
+++ .github/testScripts/test.sh
@@ -5,7 +5,6 @@
 # If there is a difference, script will exit with 1, i.e., error out
 
 curl -o ./.github/testScripts/actual.html http://127.0.0.1:8080/sample/index.html && \
-echo "PURPOSEFULLY WRONG TO SHOW TEST FAILING" > ./.github/testScripts/expected.html
 diff ./.github/testScripts/expected.html ./.github/testScripts/actual.html && \
 cmp ./.github/testScripts/expected.html ./.github/testScripts/actual.html && \
 echo Tomcat Server OK
