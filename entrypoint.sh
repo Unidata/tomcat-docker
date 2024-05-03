4b4f14c057b935d9d41e3b62b58839c2cb042169
Julien Chastang <julien.c.chastang@gmail.com>
2024-05-03 14:26:54 -0600

    addressing a permissions issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,8 +21,8 @@ if [ "$1" = 'start-tomcat.sh' ] || [ "$1" = 'catalina.sh' ]; then
     # Change CATALINA_HOME ownership to tomcat user and tomcat group
     # Restrict permissions on conf
     ###
-
-    chown -R tomcat:tomcat ${CATALINA_HOME} && chmod 400 ${CATALINA_HOME}/conf/*
+    chown -R tomcat:tomcat ${CATALINA_HOME} && find ${CATALINA_HOME}/conf \
+        -type d -exec chmod 755 {} \; -o -type f -exec chmod 400 {} \;
     sync
 
     ###
